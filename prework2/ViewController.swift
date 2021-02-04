19104beddc6562e550354cbe3a1553f3ce5b4fc6
Weijun Fan <fwrrong@163.com>
2021-02-05 04:44:32 +0800

    Initial Commit

---
+++ prework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prework2
+//
+//  Created by 范为君 on 2021/2/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
